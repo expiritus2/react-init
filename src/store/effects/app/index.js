80a351a1a0718695498dd82cb2941fbff4daa08c
Mikhail <michaildrazdovich@gmail.com>
2020-11-17 22:34:58 +0300

    Move effect ections to effects

--- src/store/effects/app/index.js
+++ src/store/effects/app/index.js
@@ -1,5 +1,5 @@
 import * as api from 'store/effects/core';
 import { getTestPosts } from 'api/app';
-import { AppActions } from 'store/actions/app';
+import { AppActions } from 'store/actions';
 
 export const load = api.execBase(AppActions.getTest, getTestPosts);
